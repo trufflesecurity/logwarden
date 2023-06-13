cb6107e17daa39d0b0d63e772318010868a71690
Dustin Decker <dustin@trufflesec.com>
2023-06-13 10:00:45 -0700

    add healthcheck port

--- Makefile
+++ Makefile
@@ -8,3 +8,8 @@ fmt:
 
 lint:
 	golangci-lint run --enable bodyclose --enable exportloopref --out-format=colored-line-number --timeout 10m
+
+docker:
+	docker buildx build --push \
+		--platform linux/amd64,linux/arm64 \
+		--tag us-docker.pkg.dev/thog-artifacts/public/logwarden:latest .
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/trufflesecurity/logwarden/internal/engine"
@@ -70,6 +71,17 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	}()
+
+	go func() {
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+		}
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		log.Fatal(http.ListenAndServe(":"+port, nil))
 	}()
 
 	err = eng.Subscribe(ctx, *project, *subscription)
